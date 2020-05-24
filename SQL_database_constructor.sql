742a44bb180404c55240a597f79e3615f93d855d
Rafa Garrido <draco032@gmail.com>
2020-05-24 00:39:17 -0400

    Add SQL_database_constructor.sql

---
+++ SQL_database_constructor.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2020 at 06:32 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `moviedata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actors`
+--
+
+CREATE TABLE `actors` (
+  `actorID` int(11) NOT NULL,
+  `photo` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `dateOfBirth` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `casting`
+--
+
+CREATE TABLE `casting` (
+  `movieID` int(11) NOT NULL,
+  `actorID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genrelist`
+--
+
+CREATE TABLE `genrelist` (
+  `movieID` int(11) NOT NULL,
+  `genreID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `genreID` int(11) NOT NULL,
+  `genreName` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `movieID` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `year` year(4) DEFAULT NULL,
+  `cover` varchar(50) DEFAULT NULL,
+  `youtubelink` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `actors`
+--
+ALTER TABLE `actors`
+  ADD PRIMARY KEY (`actorID`);
+
+--
+-- Indexes for table `casting`
+--
+ALTER TABLE `casting`
+  ADD PRIMARY KEY (`movieID`,`actorID`),
+  ADD KEY `actorID` (`actorID`);
+
+--
+-- Indexes for table `genrelist`
+--
+ALTER TABLE `genrelist`
+  ADD PRIMARY KEY (`genreID`,`movieID`),
+  ADD KEY `movieID` (`movieID`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genreID`);
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`movieID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `actors`
+--
+ALTER TABLE `actors`
+  MODIFY `actorID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `genreID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `movieID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `casting`
+--
+ALTER TABLE `casting`
+  ADD CONSTRAINT `casting_ibfk_1` FOREIGN KEY (`movieID`) REFERENCES `movies` (`movieID`),
+  ADD CONSTRAINT `casting_ibfk_2` FOREIGN KEY (`actorID`) REFERENCES `actors` (`actorID`);
+
+--
+-- Constraints for table `genrelist`
+--
+ALTER TABLE `genrelist`
+  ADD CONSTRAINT `genrelist_ibfk_1` FOREIGN KEY (`genreID`) REFERENCES `genres` (`genreID`),
+  ADD CONSTRAINT `genrelist_ibfk_2` FOREIGN KEY (`movieID`) REFERENCES `movies` (`movieID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
