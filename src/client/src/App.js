84515033a94a0e4eefccd0bcf881cef7097ac7c9
Rafa Garrido <draco032@gmail.com>
2020-06-01 15:02:38 -0400

    Add Actor, Movies components

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react';
 
 //Samples
 import movies from './sample/sampleMovies.json'
+import actors from './sample/sampleActors.json'
 
 //Components
 import Navigation from "./components/Navigation";
 import Movie from "./components/Movie"
+import Movies from "./components/Movies"
+import Actor from "./components/Actor"
+import Actors from "./components/Actors"
 
 
 export default class App extends Component {
   state = {
-    movies: movies
+    movies: movies,
+    actors: actors
   }
 
   render() {
     return (
       <div className="App">
-
-        {/*<Navigation/>*/}
-        {this.state.movies.map( movie => <Movie movie={movie} /> )}
-        
+       
       </div>
     );
   }
 }
+
+//Testing components
+/*
+<Movies movies={this.state.movies} />
+<Actors actors={actors}/>
+<Navigation/>
+*/
---
+++ src/client/src/components/Actors.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import Actor from './Actor'
+
+
+export default class Actors extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.actors.map( actor => <Actor actor={actor} /> )}
+            </div>
+        )
+    }
+}
