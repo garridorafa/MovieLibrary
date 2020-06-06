678c7e1b688066fb2f4c305e1ee1075eea4e37ad
Rafa Garrido <draco032@gmail.com>
2020-06-06 00:21:17 -0400

    Add server cors

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = 8080;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
